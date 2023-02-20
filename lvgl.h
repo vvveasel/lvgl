b6c837f4dfcadaeb7ef6f2c1ec81cd41ca8a439a
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2023-02-20 20:29:58 +0100

    add only warnings about the dev version

--- lvgl.h
+++ lvgl.h
@@ -117,7 +117,7 @@ extern "C" {
  *      DEFINES
  *********************/
 #ifndef LV_USE_DEV_VERSION
-#error "You are using the development version of LVGL which is not stable at this moment. For production use the release/v8.3 branch. To develop LVGL add #define LV_USE_DEV_VERSION to lv_conf.h"
+#warning "You are using the development version of LVGL which is not stable at this moment. For production use the release/v8.3 branch. To silence this warning add #define LV_USE_DEV_VERSION to lv_conf.h"
 #endif
 
 /**********************
